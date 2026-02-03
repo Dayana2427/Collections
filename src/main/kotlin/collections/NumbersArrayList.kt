3f8a3d91d24e594d45750fac022ed80afa7bb2cc
Karen Bolaños <dayana0212b@gmail.com>
2026-02-03 14:45:12 -0500

    Added arary list part2

--- src/main/kotlin/collections/NumbersArrayList.kt
+++ src/main/kotlin/collections/NumbersArrayList.kt
@@ -2,14 +2,65 @@ package collections
 
 class NumbersArrayList : NumbersMutableList {
 
-    private val numbers = arrayOfNulls<Int>(10)
+    private var numbers = arrayOfNulls<Int>(10)
 
     override var size: Int = 0
         private set
 
     override fun add(number: Int) {
+        growIfNeeded()
         numbers[size] = number
         size++
+    }
+
+    private fun growIfNeeded(){
+        if (numbers.size == size) {
+            val newArray = arrayOfNulls<Int>(numbers.size * 2)
+            for (index in numbers.indices) {
+                newArray[index] = numbers[index]
+            }
+            numbers = newArray
+        }
+    }
+
+    override fun add(index: Int, number: Int) {
+        growIfNeeded()
+        for (i in size downTo index + 1) {
+            numbers[i] = numbers[i + 1]
+        }
+        numbers[index] = number
+        size++
+    }
+
+    override fun removeAt(index: Int) {
+        for (i in index until size - 1){
+            numbers[i] = numbers[i + 1]
+        }
+        size--
+        numbers[size] = null
+    }
+
+    override fun clear() {
+        numbers = arrayOfNulls<Int>(10)
+        size = 0
+    }
+
+    override fun contains(number: Int): Boolean {
+        for (i in 0 until size) {
+            if (numbers[i] == number) {
+                return true
+            }
+        }
+        return false
+    }
+
+    override fun remove(number: Int) {
+        for (i in 0 until size) {
+            if (numbers[i] == number) {
+                removeAt(i)
+                return
+            }
+         }
     }
 
     override fun get(index: Int): Int {
--- src/test/kotlin/collections/NumbersMutableListTest.kt
+++ src/test/kotlin/collections/NumbersMutableListTest.kt
@@ -5,11 +5,60 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
 
 class NumbersMutableListTest {
+
     @ParameterizedTest
     @MethodSource("mutableListSource")
     fun`When add 1 element then size 1`(list: NumbersMutableList) {
         list.add(0)
         assertEquals(1, list.size)
+    }
+
+    @ParameterizedTest
+    @MethodSource("mutableListSource")
+    fun `When list is cleared the size is  elements`(list: NumbersMutableList) {
+        repeat(100) {
+            list.add(it)
+        }
+        list.clear()
+        assertEquals(0, list.size)
+    }
+
+    @ParameterizedTest
+    @MethodSource("mutableListSource")
+    fun `When list contains element then method returns true`(list: NumbersMutableList) {
+        repeat(100) {
+            list.add(it)
+        }
+        assertTrue(list.contains(90))
+    }
+
+    @ParameterizedTest
+    @MethodSource("mutableListSource")
+    fun `When list does not contains element then method returns false`(list: NumbersMutableList) {
+        repeat(100) {
+            list.add(it)
+        }
+        assertFalse(list.contains(100))
+    }
+
+    @ParameterizedTest
+    @MethodSource("mutableListSource")
+    fun `When element added to first position then in is in first position`(list: NumbersMutableList) {
+        repeat(100){
+            list.add(it)
+        }
+        list.add(0, 1000)
+        assertEquals(1000, list.get(0))
+    }
+
+    @ParameterizedTest
+    @MethodSource("mutableListSource")
+    fun `When element added to last position then in is in last position`(list: NumbersMutableList) {
+        repeat(100){
+            list.add(it)
+        }
+        list.add(100, 1000)
+        assertEquals(1000, list.get(100))
     }
 
     @ParameterizedTest
@@ -23,12 +72,66 @@ class NumbersMutableListTest {
 
     @ParameterizedTest
     @MethodSource("mutableListSource")
+    fun`When add 100 element then size 100`(list: NumbersMutableList) {
+        repeat(100) {
+           list.add(it)
+        }
+        assertEquals(100, list.size)
+    }
+
+    @ParameterizedTest
+    @MethodSource("mutableListSource")
     fun`When get 5th elemet the result is correct`(list: NumbersMutableList) {
         repeat(10) {
             list.add(it)
         }
         assertEquals(5, list.get(5))
     }
+
+    @ParameterizedTest
+    @MethodSource("mutableListSource")
+    fun`When get 50th elemet the result is correct`(list: NumbersMutableList) {
+        repeat(100) {
+            list.add(it)
+        }
+        assertEquals(50, list.get(50))
+    }
+
+    @ParameterizedTest
+    @MethodSource("mutableListSource")
+    fun `When  element removed then size decreased`(list: NumbersMutableList) {
+        repeat(100) {
+            list.add(it)
+        }
+        list.removeAt(50)
+        assertEquals(99, list.size)
+    }
+
+    @ParameterizedTest
+    @MethodSource("mutableListSource")
+    fun `When removed 50th element next value at this position`(list: NumbersMutableList) {
+        repeat(100) {
+            list.add(it)
+        }
+        list.removeAt(50)
+        assertEquals(51, list.get(50))
+    }
+
+    @ParameterizedTest
+    @MethodSource("mutableListSource")
+    fun `When removed value 50 next value at this position`(list: NumbersMutableList) {
+        repeat(100) {
+            list.add(it)
+        }
+        list.remove(50)
+        assertEquals(51, list.get(50))
+    }
+
+
+
+
+
+
 
     companion object {
 
